dd03d1cd21f1ca8ab1596cc3c35d2f12be9fb662
Queenie-101 <queenestheradekanmi@gmail.com>
2026-02-01 17:54:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session
 app=Flask(__name__)
+#secret key
+app.secret_key="secretkey"
+#temporary storage for users
+users={}
 #routing-display the home page first
 @app.route('/',methods=['GET','POST'])
 #function to login
@@ -8,8 +12,12 @@ def login():
         #username and password
         username=request.form['username']
         password=request.form['password']
-        #redirect the user
-        return redirect(url_for('success',name=username,passw=password))
+        if username in users and users[username]==password:
+            session['username']=username#add the user to session
+            #redirect the user  
+            return redirect(url_for('success',name=username,passw=password))
+        else:
+            return "Invalid username or password. Please try again."
     return render_template('home.html')
 #dashboard function
 @app.route('/success/<name>/<passw>')
@@ -19,5 +27,20 @@ def success(name,passw):
 @app.route('/logout')
 def logout():
     return redirect(url_for('login'))
+#sign up function
+@app.route('/signuo',methods=['GET','POST'])
+def signup():
+    if request.method=='POST':
+        #get the username and password
+        username=request.form['username']
+        password=request.form['password']
+        if username in users:
+            return "Username already exists. Please choose a different username."
+        else:
+            #store them in the users dictionary
+            users[username]=password
+            #redirect to the login page
+            return redirect(url_for('login'))
+    return render_template('signup.html')
 if __name__=='__main__':
     app.run(debug=True)
